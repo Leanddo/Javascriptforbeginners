5048782ed11044e5aa937929f106c80fc72841c9
Leandro Amaral <138171959+Leanddo@users.noreply.github.com>
2023-08-04 17:17:05 +0000

    Add files via upload

---
+++ javascript/moduloD/ex001.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log('A velocidade ddo seu carro é ' + vel + ' km/h')
+if (vel > 60) {
+    console.log('Ultrapassasye o limiter de velocidade permitida. SERAS MULTADOD!')
+} else {
+    console.log('ditija sempre com o cinto de segurança')
+}
